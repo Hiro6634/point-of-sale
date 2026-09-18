d78645306215ccc306dec80737a80d4650fee1e9
esuyama <eduardo.suyama@flockit.com.ar>
2026-09-17 22:46:13 -0300

    HU-001: finalizado la app instalable

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,9 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+  })
+}
